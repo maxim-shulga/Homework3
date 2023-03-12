4af115174df0af52c2d547585a0889478a522517
maxim <maxim.shulga.p@gmail.com>
2023-03-12 19:12:19 +0200

    task2, added constructors with different parameters for clas ClassRoom

--- src/task2/ClassRoom.java
+++ src/task2/ClassRoom.java
@@ -1,14 +1,26 @@
 package task2;
 
 public class ClassRoom {
-    Pupil[] pupils = new Pupil[4];
+    Pupil[] pupils;
     public ClassRoom(Pupil pupil1, Pupil pupil2, Pupil pupil3, Pupil pupil4){
+        pupils = new Pupil[4];
         pupils[0] = pupil1;
         pupils[1] = pupil2;
         pupils[2] = pupil3;
         pupils[3] = pupil4;
-
     }
+    public ClassRoom(Pupil pupil1, Pupil pupil2, Pupil pupil3){
+        pupils = new Pupil[3];
+        pupils[0] = pupil1;
+        pupils[1] = pupil2;
+        pupils[2] = pupil3;
+    }
+    public ClassRoom(Pupil pupil1, Pupil pupil2){
+        pupils = new Pupil[2];
+        pupils[0] = pupil1;
+        pupils[1] = pupil2;
+    }
+
     public void showPupils(){
         for (Pupil pupil: pupils
              ) {
